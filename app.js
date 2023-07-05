c5f5749006c85eb4f68e703d8b888cd1a8113715
Fumi <>
2023-07-05 10:36:25 +0900

    追加：初期ファイル一式

---
+++ app.js
@@ -0,0 +1,23 @@
+import mysql from "mysql";
+import express from "express";
+
+// 環境変数を使用してDBにアクセスする
+const pool = mysql.createPool({
+  port: 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+
+// HTTPサーバを起動する
+const port = process.env.SERVER_PORT || 8000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
