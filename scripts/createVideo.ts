f0c54cf29ddc33d0b967a1560bc49282b2142e2c
masunaga <dalianese0@gmail.com>
2023-07-12 23:17:57 +0900

    Videoの作成（C612032023）

---
+++ scripts/createVideo.ts
@@ -0,0 +1,21 @@
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+async function videoCreate() {
+  const video = await prisma.video.create({
+    data: {
+      name: "video 1",
+      description: "The very first video in the whole site.",
+      published: true
+    }
+  })
+  console.log(video);
+}
+
+videoCreate()
+  .catch(e => {
+    console.log(e.message)
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
