26acf9506c251be179b0395c3bd7334b5d60dcff
Fumi <>
2023-07-14 13:33:29 +0900

    ルーティング＆動作確認完了

--- scripts/createCourse.ts
+++ scripts/createCourse.ts
@@ -3,13 +3,14 @@ const prisma = new PrismaClient();
 
 export async function createCourse() {
   try {
-    await prisma.course.create({
+    const createdCourse = await prisma.course.create({
       data: {
         name: 'Course Name',
         description: 'Enjoy the course!',
         published: true,
       },
     });
+    console.log(createdCourse);
   } catch (e: any) {
     console.log(e.message);
   } finally {
