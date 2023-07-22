2f2ec9af7024a5cbbad5f407bd5b9e91f610f729
masunaga <dalianese0@gmail.com>
2023-07-22 10:25:10 +0900

    お問い合わせのバリデーション実装

--- scripts/createContact.ts
+++ scripts/createContact.ts
@@ -5,15 +5,6 @@ const prisma = new PrismaClient();
 
 export async function createContact() {
   try {
-    // データのバリデーションルールを定義
-    const contactValidationRules = [
-      check('name').isEmpty().withMessage('名前は必須です。'),
-      check('email').isEmpty().withMessage('メールアドレスは必須です。').not().isEmail().withMessage('有効なメールアドレスの形式で入力してください。'),
-      check('subject').isEmpty().withMessage('件名は必須です。'),
-      check('message').isEmpty().withMessage('メッセージは必須です。'),
-      check('status').isIn([ 0 ]).withMessage('無効なステータス値です。'),
-    ];
-
     // 型宣言と実際のデータを設定
     type Contact = {name?: string, email: string, subject: string, message: string, status: number};
     const contactData: Contact = {
@@ -24,14 +15,8 @@ export async function createContact() {
       status: 0,
     };
 
-    // バリデーションの実行
-    for (const validationRule of contactValidationRules) {
-      validationRule.run(contactData);
-    }
-
     // エラーを取得
     const errors = validationResult(contactData);
-    console.log("errors", errors);
 
     // エラーが発生していたらエラー内容を取得
     if (!errors.isEmpty()) {
@@ -47,7 +32,6 @@ export async function createContact() {
           status: contactData.status,
         },
       });
-      console.log("createdContact", createdContact);
     }
 
   } catch (e: any) {
