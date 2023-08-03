78e53bc67b6bb3eeb9fe604f8cb1baf121716ae7
masunaga <dalianese0@gmail.com>
2023-08-03 11:09:05 +0900

    UI作成-useSWR修正中

--- frontend/views/ContactList.tsx
+++ frontend/views/ContactList.tsx
@@ -1,17 +1,26 @@
-import { FC } from 'react'
 import useSWR from 'swr'
 
-export const ContactList: FC = () => {
+export async function ContactList() {
+  console.log('aaaaa')
   const fetcher = () => fetch('http://localhost:8000/admin/contacts')
-  const { data, error } = useSWR('http://localhost:3000', fetcher)
+  const data = await fetcher()
+  return { props: { data } }
 
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  function CheckContactList(props) {
+    const initialData = props.data
+    const { data, error } = useSWR('http://localhost:3000', fetcher, {
+      initialData,
+    })
+    console.log('data', data)
 
-  return (
-    <div>
-      <p>お問い合わせ一覧</p>
-      <p>{data}</p>
-    </div>
-  )
+    if (error) return <div>failed to load</div>
+    if (!data) return <div>loading...</div>
+
+    return (
+      <div>
+        <p>お問い合わせ一覧</p>
+        <p>{data}</p>
+      </div>
+    )
+  }
 }
