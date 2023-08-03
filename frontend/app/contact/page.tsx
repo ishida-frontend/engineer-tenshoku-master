a5432b7a4526d22ae88013f64ffff16681d46c01
issiy <allmylifewriter@gmail.com>
2023-08-04 00:22:26 +0900

    contaxtで画面が表示できるように修正

---
+++ frontend/app/contact/page.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { useState } from 'react'
+import { Box, Button, Stack, Text } from '@chakra-ui/react'
+import { ContactList } from '../../components/organisms/ContactList'
+import React from 'react'
+
+export default function Page() {
+  const [fontSize, setFontSize] = useState(16)
+
+  const increase = () => {
+    setFontSize((prev) => Math.min(50, prev + 2))
+  }
+
+  const decrease = () => {
+    setFontSize((prev) => Math.max(10, prev - 2))
+  }
+
+  return (
+    <>
+      <h1>お問い合わせ一覧</h1>
+      <Box>
+        <Stack direction="row" spacing={4}>
+          <Button bg="teal.300" color="white" onClick={increase}>
+            拡大
+          </Button>
+          <Button bg="red.300" color="white" onClick={decrease}>
+            縮小
+          </Button>
+        </Stack>
+        <Text fontSize={fontSize + 'px'}>
+          上のボタンを押すと、このテキストのサイズが変わります(最小サイズ：10px・最大サイズ：50px)。
+        </Text>
+
+        <ContactList />
+      </Box>
+    </>
+  )
+}
