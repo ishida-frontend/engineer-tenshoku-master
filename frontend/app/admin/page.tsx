c83ffb3ee740b09882ac8bf5568a5a2a5a7c9b89
masunaga <dalianese0@gmail.com>
2023-08-04 22:28:01 +0900

    /admin画面仮作成

---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React from 'react'
+import { Box } from '@chakra-ui/react'
+import AdminHome from '../../components/admin/page'
+
+export default function Page() {
+  return (
+    <>
+      <Box>
+        <AdminHome />
+      </Box>
+    </>
+  )
+}
