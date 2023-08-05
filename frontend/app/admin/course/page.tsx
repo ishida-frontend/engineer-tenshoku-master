e9d163216dbbe7ced61bbe7444d11d8d96b435b6
Fumi <>
2023-08-05 16:16:29 +0900

    【管理画面】教材一覧の対応

---
+++ frontend/app/admin/course/page.tsx
@@ -0,0 +1,7 @@
+'use client'
+import React from 'react'
+import { CourseList } from '../../../components/organisms/CourseList'
+
+export default function AdminCourseList() {
+  return <CourseList />
+}
