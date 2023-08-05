b49b7eb4762811676674a4df0efefd0030be6e2f
Fumi <>
2023-08-05 12:09:46 +0900

    componentsディレクトリを/frontend配下へ移動

--- frontend/app/course/page.tsx
+++ frontend/app/course/page.tsx
@@ -1,7 +1,6 @@
 'use client'
-import React from "react"
-import { CourseList } from "../components/organisms/CourseList"
-
+import React from 'react'
+import { CourseList } from '../../components/organisms/CourseList'
 
 export default function Course() {
   return (
