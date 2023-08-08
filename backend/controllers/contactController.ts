980c957d139308500bcda46d1b22746fc5cdc54d
masunaga <dalianese0@gmail.com>
2023-08-08 13:20:42 +0900

    質問：createContact.tsでのPOSTデータ取得方法

--- backend/controllers/contactController.ts
+++ backend/controllers/contactController.ts
@@ -15,7 +15,7 @@ exports.checkCreateContact = async function (
   // TODO createContact()でデータ取得してから、バリデーションを実行しないとvalueが空になる(フロントが出来上がってから要対応)
   try {
     console.log('req.body', req.body)
-    await createContact()
+    await createContact(req.body)
     res.send('新しいお問い合わせが作成されました！')
   } catch (e: any) {
     res.status(500).send('エラーが発生しました')
