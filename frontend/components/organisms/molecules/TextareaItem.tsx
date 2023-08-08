2c1dd3f488879cd9da46947706dc90b5791930d8
masunaga <dalianese0@gmail.com>
2023-08-08 20:12:21 +0900

    widthとheightを削除しました

--- frontend/components/organisms/molecules/TextareaItem.tsx
+++ frontend/components/organisms/molecules/TextareaItem.tsx
@@ -5,18 +5,16 @@ interface Props {
   name: string
   value: string
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
-  height: string
   placeholder: string
 }
 
 const TextareaItem: FC<Props> = (props) => {
-  const { value, onChange, height, placeholder } = props
+  const { value, onChange, placeholder } = props
   return (
     <>
       <Textarea
         value={value}
         onChange={(e) => onChange(e)}
-        height={height}
         placeholder={placeholder}
       ></Textarea>
     </>
