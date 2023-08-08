5c354e1112d3dba782aa1a5c01d74eeb56004c07
masunaga <dalianese0@gmail.com>
2023-08-08 19:09:43 +0900

    お問い合わせフォームのデザイン反映

--- frontend/components/organisms/molecules/TextareaItem.tsx
+++ frontend/components/organisms/molecules/TextareaItem.tsx
@@ -1,16 +1,24 @@
 import React, { FC, useState } from 'react'
+import { Textarea } from '@chakra-ui/react'
 
 interface Props {
   name: string
   value: string
   onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
+  height: string
+  placeholder: string
 }
 
 const TextareaItem: FC<Props> = (props) => {
-  const { value, onChange } = props
+  const { value, onChange, height, placeholder } = props
   return (
     <>
-      <textarea value={value} onChange={(e) => onChange(e)}></textarea>
+      <Textarea
+        value={value}
+        onChange={(e) => onChange(e)}
+        height={height}
+        placeholder={placeholder}
+      ></Textarea>
     </>
   )
 }
