f881b58bbcc22d2664a0e41814c383b1fb2b9445
issiy <allmylifewriter@gmail.com>
2023-08-12 19:59:35 +0900

    validationがバックエンド側でもできるように

--- backend/validation.ts
+++ backend/validation.ts
@@ -1,30 +1,29 @@
-import { check, validationResult } from 'express-validator'
-import { z } from 'zod'
+import { AnyZodObject, ZodError, z } from 'zod'
 import express, { Request, Response, NextFunction } from 'express'
 
-export const contactValidationRules = (req: Request, res: Response) => {
-  z.object({
-    name: z.string().min(5, { message: '名前は5文字以上必要です。' }),
-    email: z
-      .string()
-      .min(5, { message: 'メールは5文字以上必要です。' })
-      .email({ message: '有効なメールアドレスの形式で入力してください。' }),
-    subject: z.string().min(6, { message: 'タイトルは6文字以上必要です。' }),
-    message: z.string().min(6, { message: '内容は6文字以上必要です。' }),
-  })
-}
+export const validate =
+  (schema: AnyZodObject) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await schema.parseAsync({ ...req.body })
+      return next()
+    } catch (error) {
+      console.log('error', error)
+      if (error instanceof ZodError) {
+        console.log('e')
+        return res.status(500).send({
+          error: error.flatten(),
+        })
+      }
+    }
+  }
 
-// export type ContactValidationRules = z.infer<typeof contactValidationRules>
-
-// export const contactValidationRules = [
-//   check('name').not().isEmpty().withMessage('名前は必須です。'),
-//   check('email')
-//     .not()
-//     .isEmpty()
-//     .withMessage('メールアドレスは必須です。')
-//     .isEmail()
-//     .withMessage('有効なメールアドレスの形式で入力してください。'),
-//   check('subject').not().isEmpty().withMessage('件名は必須です。'),
-//   check('message').not().isEmpty().withMessage('メッセージは必須です。'),
-//   check('status').isIn([0]).withMessage('無効なステータス値です。'),
-// ]
+export const contactValidationRules = z.object({
+  name: z.string().min(5, { message: '名前は5文字以上必要です。' }),
+  email: z
+    .string()
+    .min(5, { message: 'メールは5文字以上必要です。' })
+    .email({ message: '有効なメールアドレスの形式で入力してください。' }),
+  subject: z.string().min(6, { message: 'タイトルは6文字以上必要です。' }),
+  message: z.string().min(6, { message: '内容は6文字以上必要です。' }),
+})
