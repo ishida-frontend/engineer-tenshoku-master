16dbb932640942bf5188e9403197a358dbe89d57
takashi sano <takashisn319@gmail.com>
2023-08-13 10:51:36 +0900

    Tag一覧機能追加

--- backend/scripts/readTag.ts
+++ backend/scripts/readTag.ts
@@ -2,38 +2,52 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function readTag() {
-   const tag = await prisma.tag.findUnique({
+  const tag = await prisma.tag.findUnique({
     where: {
-      id: 99,
+      id: 1,
     },
   })
-  console.dir(tag, { depth: null })
+  console.log("readTag", tag)
 }
 
 readTag()
-.catch(e => {
-  throw e
-})
-.finally(async () => {
-  await prisma.$disconnect()
-})
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
+
+export async function readAllTag() {
+  const tag = await prisma.tag.findMany({})
+  console.log("readAllTag", tag)
+}
+
+readAllTag()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
 
 export async function readFilteredTag() {
-const tag = await prisma.tag.findMany({
-  where: {
-    AND: {
-      id: 99,
-      name:'github'
-    }
-  }
-})
-console.dir(tag, { depth: null })
+  const tag = await prisma.tag.findMany({
+    where: {
+      AND: {
+        id: 1,
+        name: 'github',
+        color: 'black',
+      },
+    },
+  })
+  console.log("readFilteredTag", tag)
 }
 
 readFilteredTag()
-.catch(e => {
-  throw e
-})
-.finally(async () => {
-  await prisma.$disconnect()
-})
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
