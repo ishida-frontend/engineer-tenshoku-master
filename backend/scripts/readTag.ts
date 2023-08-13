369a70e36c449a72a7c5dddc022430a725af3339
takashi sano <takashisn319@gmail.com>
2023-08-13 09:35:54 +0900

    no message

--- backend/scripts/readTag.ts
+++ backend/scripts/readTag.ts
@@ -2,13 +2,38 @@ import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
 export async function readTag() {
-  const tag = await prisma.tag.read({
-    data: {
-      name: 'githab',
-      color: 'black',
+   const tag = await prisma.tag.findUnique({
+    where: {
+      id: 99,
     },
   })
-  return tag
+  console.dir(tag, { depth: null })
 }
 
 readTag()
+.catch(e => {
+  throw e
+})
+.finally(async () => {
+  await prisma.$disconnect()
+})
+
+export async function readFilteredTag() {
+const tag = await prisma.tag.findMany({
+  where: {
+    AND: {
+      id: 99,
+      name:'github'
+    }
+  }
+})
+console.dir(tag, { depth: null })
+}
+
+readFilteredTag()
+.catch(e => {
+  throw e
+})
+.finally(async () => {
+  await prisma.$disconnect()
+})
