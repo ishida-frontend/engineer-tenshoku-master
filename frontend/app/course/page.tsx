bf08b2196ce174b44cacaeba777b8eb668fbe50f
Fumi <>
2023-08-19 00:40:25 +0900

    教材一覧ページデザイン反映

--- frontend/app/course/page.tsx
+++ frontend/app/course/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React from 'react'
+
 import { CourseList } from '../../components/organisms/CourseList'
 
 export default function Course() {
--- frontend/components/organisms/CourseList.tsx
+++ frontend/components/organisms/CourseList.tsx
@@ -1,32 +1,125 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import useSWR from 'swr'
+import {
+  Box,
+  Card,
+  CardBody,
+  CardHeader,
+  ChakraProvider,
+  extendTheme,
+  Flex,
+  Heading,
+  Icon,
+  Image,
+  Link,
+  SimpleGrid,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import { BsChevronRight } from 'react-icons/bs'
+import { RiReactjsLine } from 'react-icons/ri'
 
 import { CourseType } from '../../types'
+import { Loader } from '../atoms/Loader'
+import { PRIMARY_FONT_COLOR } from '../../constants/colors'
+import { useCustomToast } from '../../hooks/useCustomToast'
 
 export function CourseList() {
+  // カスタムフック準備
+  const { showErrorToast } = useCustomToast()
+
+  const courseListTheme = extendTheme({
+    breakpoints: {
+      md: '700px',
+      lg: '1050px',
+    },
+  })
+
   const fetcher = async () =>
-    (await fetch('http://localhost:8000/course/all')).json()
+    (await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/course/all`)).json()
 
-  const { data, error } = useSWR('courseList', fetcher)
+  const { data: courses, error } = useSWR('courseList', fetcher)
 
-  if (error) return <div>コースの取得に失敗しました。</div>
-  if (!data) return <div>読み込み中……</div>
+  // ページを開いて１０秒でデータ取得ができなかった場合のエラートースト
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (error) {
+        showErrorToast('コースの取得に失敗しました。')
+      }
+    }, 10000)
+    return () => clearTimeout(timeout)
+  }, [courses])
+
+  // コースデータ読み込みアニメーション
+  if (!courses) return <Loader />
 
   return (
-    <div>
-      <h1>コース一覧</h1>
-      {data.map((course: CourseType) => {
-        return (
-          <div key={course.id}>
-            <p>コースID：{course.id}</p>
-            <p>コース名：{course.name}</p>
-            <p>概要：{course.description}</p>
-            <p>作成日：{course.created_at}</p>
-            <p>更新日：{course.updated_at}</p>
-          </div>
-        )
-      })}
-    </div>
+    <ChakraProvider theme={courseListTheme}>
+      <VStack mx="auto">
+        <Heading py={10} color={PRIMARY_FONT_COLOR} fontSize="36px">
+          コース一覧
+        </Heading>
+        <Box mb={10}>
+          <Flex
+            alignItems="center"
+            pb={2}
+            borderBottom="1px"
+            borderColor={'blackAlpha.500'}
+          >
+            <Icon as={RiReactjsLine} fontSize={20} />
+            <Text ml={2} fontSize={16} fontWeight="bold">
+              React
+            </Text>
+          </Flex>
+          <SimpleGrid
+            columns={{ base: 1, md: 2, lg: 3 }}
+            mt="40px"
+            spacingX={[0, 10, 20]}
+            spacingY="10"
+          >
+            {courses.map((course: CourseType) => (
+              <Card key={course.id} w="288px" boxShadow="md" borderRadius="8px">
+                <CardHeader p={0}>
+                  <Image
+                    src="/images/reactIcon.png"
+                    alt={`${course.name}のアイコン`}
+                    width="100%"
+                    height="auto"
+                  />
+                </CardHeader>
+                <CardBody px={3} py={4}>
+                  <Heading fontSize="16px">{course.name}</Heading>
+                  <Text
+                    h="34px"
+                    mt={3}
+                    lineHeight="16.94px"
+                    fontSize="14px"
+                    color={'blackAlpha.700'}
+                    overflowWrap="break-word"
+                    noOfLines={2}
+                  >
+                    {course.description}
+                  </Text>
+                  <Flex justify="flex-end">
+                    <Link
+                      href={`/course/${course.id}`}
+                      mt="2"
+                      color={PRIMARY_FONT_COLOR}
+                      _hover={{ textDecoration: 'none' }}
+                    >
+                      <Flex alignItems="center">
+                        <BsChevronRight size="20" />
+                        <Text pl={3}>もっと見る</Text>
+                      </Flex>
+                    </Link>
+                  </Flex>
+                </CardBody>
+              </Card>
+            ))}
+          </SimpleGrid>
+        </Box>
+      </VStack>
+    </ChakraProvider>
   )
 }
