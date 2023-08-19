2c0766df3a42c76d2cc460df540ed43bd00cb9ff
Fumi <>
2023-08-19 18:32:47 +0900

    コンテンツの配置完了

--- frontend/components/organisms/Footer.tsx
+++ frontend/components/organisms/Footer.tsx
@@ -1,28 +1,65 @@
 'use client'
-import { Box, Flex, Link, Text, Stack, Image } from '@chakra-ui/react'
+import {
+  Box,
+  Container,
+  extendTheme,
+  HStack,
+  Image,
+  Link,
+  Text,
+  Spacer,
+  VStack,
+  ChakraProvider,
+} from '@chakra-ui/react'
 import { AiFillYoutube, AiOutlineTwitter, AiFillFacebook } from 'react-icons/ai'
 
 export function Footer() {
+  const footerTheme = extendTheme({
+    breakpoints: {
+      sm: '500px',
+      md: '600px',
+    },
+  })
   return (
-    <Box as="footer" bgColor={'blackAlpha.800'} color="white">
-      <Stack spacing={1}>
-        <Link href="/contact">お問い合わせ</Link>
-        <Link href="#">ヘルプページ</Link>
-        <Link href="#">利用規約</Link>
-        <Link href="#">プライバシーポリシー</Link>
-        <Link href="#">特定商取引法に基づく表記</Link>
-      </Stack>
-      <Flex fontSize='30px' justify='left'>
-        <Image borderRadius='full' boxSize='48px' src='../images/jsLogo.png'  />JS学習プラットフォーム
-      </Flex>
-      <Flex justify='right'>
-        <AiFillYoutube />
-        <AiOutlineTwitter />
-        <AiFillFacebook />
-      </Flex>
-      <Text fontSize="12px">
-        &copy; {new Date().getFullYear()} JS学習プラットフォーム
-      </Text>
-    </Box>
+    <ChakraProvider theme={footerTheme}>
+      <Box as="footer" bgColor={'blackAlpha.900'} color="white">
+        <Container maxW="1024px">
+          <Box mt={12}>
+            <VStack align="left" spacing={1} fontSize="16px" lineHeight="24px">
+              <Link href="/contact" mt={12}>
+                お問い合わせ
+              </Link>
+              <Link href="#">ヘルプページ</Link>
+              <Link href="#">利用規約</Link>
+              <Link href="#">プライバシーポリシー</Link>
+              <Link href="#">特定商取引法に基づく表記</Link>
+            </VStack>
+          </Box>
+          <Box>
+            <HStack mt={10} mb={3} fontSize="30px" justify="left">
+              <Image boxSize="48px" src="../images/jslogo.png" />
+              <Text fontSize={['16px', '20px', '30px']}>
+                JS学習プラットフォーム
+              </Text>
+              <Spacer />
+              <HStack spacing="30px" fontSize="24px">
+                <Link href="#">
+                  <AiFillYoutube />
+                </Link>
+                <Link href="#">
+                  <AiOutlineTwitter />
+                </Link>
+                <Link href="#">
+                  <AiFillFacebook />
+                </Link>
+              </HStack>
+            </HStack>
+          </Box>
+          <Text align="center" fontSize="12px">
+            &copy; {new Date().getFullYear()} JS学習プラットフォーム
+          </Text>
+        </Container>
+      </Box>
+    </ChakraProvider>
   )
 }
