bcaf91dac10f7aefe3def6156a21848469d4fb93
masunaga <dalianese0@gmail.com>
2023-08-21 09:47:18 +0900

    PRお問い合わせ完了デザイン反映

--- frontend/app/contact/done/page.tsx
+++ frontend/app/contact/done/page.tsx
@@ -1,6 +1,38 @@
-// お問い合わせ送信完了
-import React, { FC } from 'react'
-import { UserContactDone } from '../../../components/organisms/ContactDone'
-const ContactDone: FC = () => <UserContactDone />
+'use client'
+import React from 'react'
+import {
+  Container,
+  Button,
+  Text,
+  Heading,
+  VStack,
+  HStack,
+  Stack,
+  extendTheme,
+} from '@chakra-ui/react'
+import Link from 'next/link'
+import { EmailIcon } from '@chakra-ui/icons'
 
-export default ContactDone
+export default function UserContactDone() {
+  return (
+    <Container bg="gray.100" maxW={'1512px'} py={'60px'}>
+      <Container bg="white" height={'723px'} maxW={'1024px'} centerContent>
+        <Heading fontSize={'2xl'} fontWeight={'bold'} mt={'80px'}>
+          お問い合わせが送信されました
+        </Heading>
+        <EmailIcon mt={'80px'} boxSize={16} color="teal" />
+        <Stack spacing={1} mt={'40px'}>
+          <Text fontSize="md">
+            お問い合わせいただき、ありがとうございます。
+          </Text>
+          <Text fontSize="md">
+            担当のものよりご連絡させていただきますので、恐れ入りますがしばらくお待ちください。
+          </Text>
+        </Stack>
+        <Button w={'140px'} mt={'60px'} colorScheme="teal">
+          <Link href="/">トップに戻る</Link>
+        </Button>
+      </Container>
+    </Container>
+  )
+}
