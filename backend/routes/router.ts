5e5891105fb0228d14d5054ba3957309946e10d8
masunaga <dalianese0@gmail.com>
2023-09-04 21:50:54 +0900

    コース詳細APIのルーティング

--- backend/routes/router.ts
+++ backend/routes/router.ts
@@ -59,7 +59,7 @@ const courseRouter = express.Router()
 router.use('/course', courseRouter)
 courseRouter.get('/create', createCourse)
 courseRouter.get('/read', readCourse)
-courseRouter.get('/get/:id', getPublishedCourse)
+courseRouter.get('/published/:id', getPublishedCourse)
 courseRouter.get('/all', readAllCourses)
 courseRouter.get('/update', updateCourse)
 
