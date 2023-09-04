33b5c514a126a9434288342f2768edbc915811a0
issiy <allmylifewriter@gmail.com>
2023-09-04 19:00:53 +0900

    ヘッダー修正途中

--- frontend/components/organisms/HeaderLoggedIn.tsx
+++ frontend/components/organisms/HeaderLoggedIn.tsx
@@ -1,13 +1,5 @@
 'use client'
-import {
-  Avatar,
-  Box,
-  Flex,
-  Heading,
-  HStack,
-  Image,
-  Spacer,
-} from '@chakra-ui/react'
+import { Avatar, Box, Flex, Heading, HStack, Spacer } from '@chakra-ui/react'
 import { AiOutlineUser } from 'react-icons/ai'
 
 import { TitleLogo } from '../atoms/TitleLogo'
@@ -17,14 +9,7 @@ export function HeaderLoggedIn() {
     <Box pt="100px" boxShadow="0 1px 3px rgba(0, 0, 0, 0.1)">
       <Box as="header" bgColor={'white'}>
         <Flex justify="center">
-          <HStack
-            maxW="1512px"
-            py="19px"
-            top="0"
-            position="fixed"
-            width="100%"
-            zIndex="10"
-          >
+          <HStack maxW="1512px" py="19px" width="100%" zIndex="10">
             <HStack spacing={0}>
               <Box pl="22px">
                 <TitleLogo boxSize="62px" />
--- frontend/components/organisms/HeaderLoggedOut.tsx
+++ frontend/components/organisms/HeaderLoggedOut.tsx
@@ -15,14 +15,7 @@ export function HeaderLoggedOut() {
   return (
     <Box pt="100px" boxShadow="0 1px 3px rgba(0, 0, 0, 0.1)">
       <Flex justify="center">
-        <HStack
-          maxW="1512px"
-          py="19px"
-          top="0"
-          position="fixed"
-          width="100%"
-          zIndex="10"
-        >
+        <HStack maxW="1512px" py="19px" width="100%" zIndex="10">
           <HStack spacing={0}>
             <Box pl="22px">
               <TitleLogo boxSize="62px" />
