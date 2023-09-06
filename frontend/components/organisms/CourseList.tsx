f1879fe1770cd9b333c8781b9e572cd29d8f690f
masunaga <dalianese0@gmail.com>
2023-09-06 14:41:55 +0900

    フッターの追加とデザイン調整

--- frontend/components/organisms/CourseList.tsx
+++ frontend/components/organisms/CourseList.tsx
@@ -56,7 +56,7 @@ export function CourseList() {
 
   return (
     <ChakraProvider theme={courseListTheme}>
-      <VStack mx="auto">
+      <VStack mx="auto" padding={'60px 90px'}>
         <Heading py={10} color={PRIMARY_FONT_COLOR} fontSize="36px">
           コース一覧
         </Heading>
