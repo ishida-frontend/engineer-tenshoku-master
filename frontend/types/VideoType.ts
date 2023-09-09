561378f1281245bcbbcc6b9926c240bb9c423e26
Fumi <>
2023-09-09 15:08:20 +0900

    型定義追加

---
+++ frontend/types/VideoType.ts
@@ -0,0 +1,12 @@
+export type VideoType = {
+  id: number
+  name: string
+  description: string
+  order: number
+  url: string
+  published: boolean
+  sectionId: number
+  created_at: string
+  updated_at: string
+  deleted_at: string
+}
--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -1,2 +1,4 @@
 export * from './CourseType'
 export * from './Contact'
+export * from './SectionType'
+export * from './VideoType'
