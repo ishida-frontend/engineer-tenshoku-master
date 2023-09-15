c228f0e837c0889d7749af9d59deb0c88e68a046
Fumi <>
2023-09-15 23:27:00 +0900

    埋め込み動画用URLへの変換ロジックの実装

--- backend/scripts/createVideo.ts
+++ backend/scripts/createVideo.ts
@@ -1,16 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 
+import { convertVideoUrl } from '../utils/convertVideoUrl'
 import { VideoType } from '../types'
 
 const prisma = new PrismaClient()
 
 export async function createVideo(videoData: VideoType) {
   try {
+    const videoUrl = convertVideoUrl(videoData.url)
     await prisma.video.create({
       data: {
         name: videoData.name,
         description: videoData.description,
-        url: videoData.url,
+        url: videoUrl,
         order: videoData.order,
         published: videoData.published,
         section: { connect: { id: videoData.sectionId } },
