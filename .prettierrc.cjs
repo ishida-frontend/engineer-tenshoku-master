6ac736de723b6f479aebc5dc486a708698d0abc2
masunaga <dalianese0@gmail.com>
2023-09-18 11:49:33 +0900

    prettierrc.cjs追加

---
+++ .prettierrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: "all",
+  printWidth: 80,
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      options: {
+        parser: "typescript",
+      },
+    },
+  ],
+};
