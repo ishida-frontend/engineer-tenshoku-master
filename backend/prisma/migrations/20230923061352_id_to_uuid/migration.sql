e31b0b0fd9b71df97721d6db7a689bcea247e871
masunaga <dalianese0@gmail.com>
2023-09-23 15:35:44 +0900

    idをuuidに変更

---
+++ backend/prisma/migrations/20230923061352_id_to_uuid/migration.sql
@@ -0,0 +1,58 @@
+/*
+  Warnings:
+
+  - The primary key for the `Contact` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Course` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Section` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Video` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - A unique constraint covering the columns `[id]` on the table `Contact` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[id]` on the table `Course` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[id]` on the table `Section` will be added. If there are existing duplicate values, this will fail.
+  - A unique constraint covering the columns `[id]` on the table `Video` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE `Section` DROP FOREIGN KEY `Section_course_id_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `Video` DROP FOREIGN KEY `Video_section_id_fkey`;
+
+-- AlterTable
+ALTER TABLE `Contact` DROP PRIMARY KEY,
+    MODIFY `id` VARCHAR(255) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- AlterTable
+ALTER TABLE `Course` DROP PRIMARY KEY,
+    MODIFY `id` VARCHAR(255) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- AlterTable
+ALTER TABLE `Section` DROP PRIMARY KEY,
+    MODIFY `id` VARCHAR(255) NOT NULL,
+    MODIFY `course_id` VARCHAR(255) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- AlterTable
+ALTER TABLE `Video` DROP PRIMARY KEY,
+    MODIFY `id` VARCHAR(255) NOT NULL,
+    MODIFY `section_id` VARCHAR(255) NOT NULL,
+    ADD PRIMARY KEY (`id`);
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Contact_id_key` ON `Contact`(`id`);
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Course_id_key` ON `Course`(`id`);
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Section_id_key` ON `Section`(`id`);
+
+-- CreateIndex
+CREATE UNIQUE INDEX `Video_id_key` ON `Video`(`id`);
+
+-- AddForeignKey
+ALTER TABLE `Section` ADD CONSTRAINT `Section_course_id_fkey` FOREIGN KEY (`course_id`) REFERENCES `Course`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Video` ADD CONSTRAINT `Video_section_id_fkey` FOREIGN KEY (`section_id`) REFERENCES `Section`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
