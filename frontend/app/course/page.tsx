6ebe31622bcfc5d8d9701a4b1a73d88d023bcba5
masunaga <dalianese0@gmail.com>
2023-10-07 20:02:34 +0900

    フロントのconsole削除

--- frontend/app/course/page.tsx
+++ frontend/app/course/page.tsx
@@ -25,7 +25,6 @@ export default async function Course() {
       <CourseListWrapper initialCourses={initialCourses} session={session} />
     )
   } catch (e) {
-    console.log(e)
     return <Error />
   }
 }
