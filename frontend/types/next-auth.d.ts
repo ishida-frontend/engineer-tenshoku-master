cf68873f353a89243964bccd85f68afff227fdcc
Fumi <issaradango@gmail.com>
2023-11-11 23:53:46 +0900

    不要なコメントの削除

--- frontend/types/next-auth.d.ts
+++ frontend/types/next-auth.d.ts
@@ -3,7 +3,6 @@ import NextAuth from 'next-auth'
 declare module 'next-auth' {
   interface Session {
     user: {
-      /** The user's Id. */
       id: string
       isAdmin: boolean
       name: string
