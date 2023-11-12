8cae9fa47250a2465974d9e191d5fd6bcd13eede
Fumi <issaradango@gmail.com>
2023-11-12 22:56:00 +0900

    動作テスト

--- frontend/app/admin/tag/page.tsx
+++ frontend/app/admin/tag/page.tsx
@@ -1,13 +1,18 @@
-'use client'
 import React from 'react'
 import { TagList } from '../../../components/admin/organisms/TagList'
 import { TagType } from '../../../types/TagType'
+import Error from '../../error'
 
 export default async function AdminTag() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tag`, {
-    cache: 'no-cache',
-  })
-  const tags: TagType[] = await res.json()
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tag`, {
+      cache: 'no-cache',
+    })
+    const tags: TagType[] = await res.json()
 
-  return <TagList tags={tags} />
+    return <TagList tags={tags} />
+  } catch (e) {
+    console.log(e)
+    return <Error />
+  }
 }
