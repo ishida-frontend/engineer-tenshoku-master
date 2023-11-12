d8d9d701d3802ccd24a59569907591c2edb47df6
Fumi <issaradango@gmail.com>
2023-11-12 20:06:26 +0900

    動作テスト

--- frontend/app/admin/tag/page.tsx
+++ frontend/app/admin/tag/page.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 import { TagList } from '../../../components/admin/organisms/TagList'
 import { TagType } from '../../../types/TagType'
 
-export default async function AdminTag() {
+export default async function AdminTag({ fetch }) {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tag`, {
+    method: 'GET',
     cache: 'no-cache',
   })
   const tags: TagType[] = await res.json()
