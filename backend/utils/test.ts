606478c70a22f952efce08375a31d67892391853
Fumi <issaradango@gmail.com>
2023-11-12 17:17:10 +0900

    動作テスト

--- backend/utils/test.ts
+++ backend/utils/test.ts
@@ -1,1 +1,2 @@
 console.log('This is a test file on the backend.')
+console.log('This is a test file on the backend.')
--- frontend/utils/test.ts
+++ frontend/utils/test.ts
@@ -1,1 +1,2 @@
 console.log('This is a test file on the frontend.')
+console.log('This is a test file on the frontend.')
