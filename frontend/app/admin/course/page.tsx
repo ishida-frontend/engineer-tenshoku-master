c48241dbf08dd79682cee917c3a75a96271e034b
Fumi <issaradango@gmail.com>
2023-11-13 14:17:43 +0900

    動作テスト

--- frontend/app/admin/course/page.tsx
+++ frontend/app/admin/course/page.tsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { CourseList } from '../../../components/admin/organisms/CourseList'
 import { CourseType } from '../../../types'
+import Error from '../../error'
 
 export default async function AdminCourse() {
-  const res = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/course`,
-    {
-      cache: 'no-cache',
-      headers: {
-        'Content-Type': 'application/json',
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/admin/course`,
+      {
+        cache: 'no-cache',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    },
-  )
-  const getCourses: CourseType[] = await res.json()
-  return <CourseList courses={getCourses} />
+    )
+    const getCourses: CourseType[] = await res.json()
+    return <CourseList courses={getCourses} />
+  } catch (e) {
+    return <Error />
+  }
 }
