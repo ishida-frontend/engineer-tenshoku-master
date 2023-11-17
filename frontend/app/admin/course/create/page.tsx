09d41e61644a2d923f9af7988ebfb4a5570ee709
Fumi <issaradango@gmail.com>
2023-11-17 19:35:34 +0900

    yarn run start (Expressサーバー立ち上げコマンド）追加

--- frontend/app/admin/course/create/page.tsx
+++ frontend/app/admin/course/create/page.tsx
@@ -1,7 +1,10 @@
 import Component from '../../../../components/admin/pages/CourseCreator'
+import Error from '../../../error'
+
 export default async function CreateCoursePage() {
   try {
     const tagsRes = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tag`, {
+      cache: 'no-cache',
       headers: {
         'Content-Type': 'application/json',
       },
@@ -9,7 +12,7 @@ export default async function CreateCoursePage() {
     const tags = await tagsRes.json()
 
     return <Component tags={tags} />
-  } catch (error) {
-    throw new Error('タグの取得に失敗しました。')
+  } catch (e) {
+    return <Error />
   }
 }
