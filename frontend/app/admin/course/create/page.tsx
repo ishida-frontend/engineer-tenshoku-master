f9cb75309db1749a237b1d512600cf4332adff00
Fumi <issaradango@gmail.com>
2023-11-17 20:23:07 +0900

    ymlファイルのリファクタリング＆不要ファイルの削除

--- frontend/app/admin/course/create/page.tsx
+++ frontend/app/admin/course/create/page.tsx
@@ -1,18 +1,13 @@
 import Component from '../../../../components/admin/pages/CourseCreator'
-import Error from '../../../error'
-
 export default async function CreateCoursePage() {
-  try {
-    const tagsRes = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tag`, {
-      cache: 'no-cache',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-    const tags = await tagsRes.json()
+  const tagsRes = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/tag`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
 
-    return <Component tags={tags} />
-  } catch (e) {
-    return <Error />
-  }
+  const tags = await tagsRes.json()
+
+  return <Component tags={tags} />
 }
