fcbaf1fdab061d0b27fa05a53ca7f8f5b31ac631
Fumi <issaradango@gmail.com>
2023-11-26 20:21:46 +0900

    CourseList関連コンポーネントのファイル名変更＆ディレクトリ変更

---
+++ frontend/components/pages/CourseList.tsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react'
+import {
+  Box,
+  Center,
+  Flex,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  VStack,
+} from '@chakra-ui/react'
+
+import { CourseListType } from '../../types'
+import { Loader } from '../atoms/Loader'
+import { PRIMARY_FONT_COLOR } from '../../constants/colors'
+import { SearchIcon } from '@chakra-ui/icons'
+import { CourseCard } from '../atoms/CourseCard'
+
+type CourseListProps = {
+  courses: CourseListType[]
+  handleTextChange: (text: string) => void
+}
+
+export function CourseList({ courses, handleTextChange }: CourseListProps) {
+  if (!courses) return <Loader />
+  const [text, setText] = useState<string>()
+
+  const handleSubmit = () => {
+    handleTextChange(text || '')
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.nativeEvent.isComposing || e.key !== 'Enter') return
+    handleSubmit()
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setText(event.target.value)
+  }
+
+  return (
+    <Center bg={'gray.200'}>
+      <VStack mx="auto" padding={'60px 96px'}>
+        <Heading py={10} color={PRIMARY_FONT_COLOR} fontSize="36px">
+          コース一覧
+        </Heading>
+        <Box marginLeft={'auto'}>
+          <InputGroup>
+            <Input
+              value={text}
+              onChange={(e) => handleInputChange(e)}
+              onKeyDown={(e) => handleKeyDown(e)}
+              variant="outline"
+              placeholder="javaScript"
+              background={'white'}
+            />
+            <InputRightElement onClick={() => handleSubmit()}>
+              <SearchIcon color="gray.500" />
+            </InputRightElement>
+          </InputGroup>
+        </Box>
+        <Box mb={10} minW={'1024px'}>
+          <Flex
+            alignItems="center"
+            pb={2}
+            borderBottom="1px"
+            borderColor={'blackAlpha.500'}
+          ></Flex>
+          {courses.length === 0 && (
+            <VStack mt="40px">
+              <Heading color={PRIMARY_FONT_COLOR} fontSize="36px">
+                検索に一致するコースはありませんでした
+              </Heading>
+            </VStack>
+          )}
+          {courses && <CourseCard courses={courses} />}
+        </Box>
+      </VStack>
+    </Center>
+  )
+}
