42279611eadca09bf38b745d5233f2f2117c0f9f
Fumi <issaradango@gmail.com>
2023-11-28 20:09:53 +0900

    Prisma Client を utils ディレクトリから呼び出すように変更

--- backend/application/answer.ts
+++ backend/application/answer.ts
@@ -1,5 +1,4 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import prisma from '../utils/prismaClient'
 
 export class AnswerApplicationService {
   static async create(body: {
