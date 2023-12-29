1c542f60dbe403ff0e45bd80acfe08ed0de03228
suzu <suzu.maryy@gmail.com>
2023-12-29 11:37:28 +0900

    LINE・実績バナーリンク追加 backend/.yarn配下のもの全て削除 frontend/components/atoms/LineBanner.tsxとAchievementBanner.tsxのコメントアウト削除

--- frontend/components/atoms/LineBanner.tsx
+++ frontend/components/atoms/LineBanner.tsx
@@ -1,18 +1,17 @@
 import { Image, Link } from '@chakra-ui/react'
 
 export const LineBanner = () => {
-  // 画像を表示するためのコンポーネント
   return (
     <Link
       href="https://utage-system.com/line/open/aEL6cT1I2zgk?mtid=qO58nDztMNCb"
       isExternal
     >
       <Image
-        src="/images/img_linebanner.png" // 画像のURLを指定
-        alt="LINEバナー" // 画像の代替テキスト
-        boxSize="210px" // 画像のサイズを指定（任意）
-        float="right" // 右寄せにするためのスタイル
-        mr={1} // 右側の余白を設定
+        src="/images/img_linebanner.png"
+        alt="LINEバナー"
+        boxSize="210px"
+        float="right"
+        mr={1}
         ml={6}
         mt={22}
         mb={4}
