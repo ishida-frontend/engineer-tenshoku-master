be71024f7c14267433da0b7ea3a32c9992620a9a
suzu <suzu.maryy@gmail.com>
2024-03-11 23:43:02 +0900

    githubフォーマットチェック修正

--- frontend/components/atoms/GoodButton.tsx
+++ frontend/components/atoms/GoodButton.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Button, HStack, Text } from '@chakra-ui/react'
+import { Button, HStack } from '@chakra-ui/react'
 import { FaRegThumbsUp, FaThumbsUp } from 'react-icons/fa'
 
 export const GoodButton = ({
