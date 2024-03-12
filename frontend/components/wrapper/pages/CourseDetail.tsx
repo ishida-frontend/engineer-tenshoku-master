5f9626c217f5bd41e4db9b897d1cbbcddab72f9d
suzu <suzu.maryy@gmail.com>
2024-03-13 00:16:41 +0900

    githubフォーマット修正

--- frontend/components/wrapper/pages/CourseDetail.tsx
+++ frontend/components/wrapper/pages/CourseDetail.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 import { Session } from 'next-auth'
-import { useRouter } from 'next/navigation'
 
 import {
   upsertViewingStatus,
