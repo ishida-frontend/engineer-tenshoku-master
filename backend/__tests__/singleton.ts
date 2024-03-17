41bc48385d822ae0623cbd94cb0b1a2fd92b51f1
aimiyamaguchi <preticure112771@gmail.com>
2024-03-17 22:43:21 +0900

    質問　createのテストコードエラー

--- backend/__tests__/singleton.ts
+++ backend/__tests__/singleton.ts
@@ -1,14 +1,18 @@
 import { mockDeep, mockReset, DeepMockProxy } from 'jest-mock-extended'
 
 import prisma, { PrismaClient } from '../utils/prismaClient'
+import { Tag } from '@prisma/client'
 
 jest.mock('../utils/prismaClient', () => ({
   __esModule: true,
-  default: mockDeep<PrismaClient>(),
+  default: mockDeep<PrismaClient>({
+    tag: mockDeep<Tag>(),
+  }),
 }))
 
 beforeEach(() => {
-  mockReset(prismaMock)
+  mockReset(prisma)
 })
 
 export const prismaMock = prisma as unknown as DeepMockProxy<PrismaClient>
+export const prismaTagMock = prisma.tag as unknown as DeepMockProxy<Tag>
