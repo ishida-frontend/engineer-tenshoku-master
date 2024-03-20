7090ec5afa4bc342398d21c73c7492536ff7544e
aimiyamaguchi <preticure112771@gmail.com>
2024-03-20 14:34:34 +0900

    レビュー修正・質問

--- backend/__tests__/medium/application-services/tag.test.ts
+++ backend/__tests__/medium/application-services/tag.test.ts
@@ -47,79 +47,94 @@ describe('広告の新規作成機能', () => {
   })
 
   test('タグの新規作成に失敗した際にエラーが投げられること', async () => {
+    const expectedError = 'エラーメッセージ'
     const errorMessage =
-      'TagApplicationService: create tag error: Error: エラーメッセージ'
-    const expectedError = new Error('エラーメッセージ')
+      'TagApplicationService: create tag error: エラーメッセージ'
     prismaMock.tag.create.mockRejectedValue(expectedError)
 
-    try {
-      await tagService.createTag(tagParams)
-
-      fail('エラーが投げられませんでした')
-    } catch (error) {
-      expect((error as Error).message).toBe(errorMessage)
-    }
+    expect.assertions(1)
+    await expect(tagService.createTag(tagParams)).rejects.toThrow(errorMessage)
   })
 })
 
-test('タグの更新が成功すること', async () => {
-  const expectedUpdatedTag = createExpectedTag({
-    name: updatedTagParams.name,
-    color: updatedTagParams.color,
-    backgroundColor: updatedTagParams.backgroundColor,
-    updated_at: new Date(),
+describe('タグの更新機能', () => {
+  test('タグの更新が成功すること', async () => {
+    const expectedUpdatedTag = createExpectedTag({
+      name: updatedTagParams.name,
+      color: updatedTagParams.color,
+      backgroundColor: updatedTagParams.backgroundColor,
+      updated_at: new Date(),
+    })
+
+    prismaMock.tag.update.mockResolvedValue(expectedUpdatedTag)
+
+    const updatedTag = await tagService.updateTag(updatedTagParams)
+    expect(updatedTag).toEqual(expectedUpdatedTag)
   })
 
-  prismaMock.tag.update.mockResolvedValue(expectedUpdatedTag)
+  test('タグの更新が失敗した際にエラーが投げられること', async () => {
+    const expectedError = 'エラーメッセージ'
+    const errorMessage = `TagApplicationService: update tag error: エラーメッセージ`
+    prismaMock.tag.update.mockRejectedValue(expectedError)
 
-  const updatedTag = await tagService.updateTag(updatedTagParams)
-  expect(updatedTag).toEqual(expectedUpdatedTag)
-})
-
-test('タグの更新が失敗した際にエラーが投げられること', async () => {
-  const errorMessage = 'タグの更新に失敗しました'
-  prismaMock.tag.update.mockRejectedValue(new Error(errorMessage))
-
-  await expect(tagService.updateTag(updatedTagParams)).rejects.toThrow(
-    errorMessage,
-  )
-})
-
-test('指定されたtagIdのタグが正しく取得されること', async () => {
-  const expectedTag = createExpectedTag()
-
-  prismaMock.tag.findUnique.mockResolvedValue(expectedTag)
-
-  const fetchedTag = await tagService.getTag(tagId)
-  expect(fetchedTag).toEqual(expectedTag)
-})
-
-test('指定されたタグの取得に失敗した際にエラーが投げられること', async () => {
-  const errorMessage = 'タグの取得に失敗しました'
-  prismaMock.tag.findUnique.mockRejectedValue(new Error(errorMessage))
-
-  await expect(tagService.getTag('invalid_tag_id')).rejects.toThrow(
-    errorMessage,
-  )
-})
-
-test('削除されていないタグが取得されること', async () => {
-  const activeTags = [
-    createExpectedTag({ id: 'tag_id_1', name: 'Tag 1', color: 'red' }),
-    createExpectedTag({ id: 'tag_id_2', name: 'Tag 2', color: 'blue' }),
-    createExpectedTag({ id: 'tag_id_3', name: 'Tag 3', color: 'green' }),
-  ]
-
-  prismaMock.tag.findMany.mockResolvedValue(activeTags)
-
-  const fetchedTags = await tagService.getTags()
-  fetchedTags.forEach((tag) => {
-    expect(tag.deleted_at).toBeNull()
+    expect.assertions(1)
+    await expect(tagService.updateTag(updatedTagParams)).rejects.toThrow(
+      errorMessage,
+    )
   })
 })
 
-test('削除されていないタグが取得されない場合、エラーが投げられること', async () => {
-  prismaMock.tag.findMany.mockResolvedValue([])
+describe('指定されたtagIdのタグを取得する機能', () => {
+  test('指定されたtagIdのタグが正しく取得されること', async () => {
+    const expectedTag = createExpectedTag()
 
-  await expect(tagService.getTags()).resolves.toEqual([])
+    prismaMock.tag.findUnique.mockResolvedValue(expectedTag)
+
+    const fetchedTag = await tagService.getTag(tagId)
+    expect(fetchedTag).toEqual(expectedTag)
+  })
+
+  test('指定されたタグの取得に失敗した際にエラーが投げられること', async () => {
+    const expectedError = 'エラーメッセージ'
+    const errorMessage = `TagApplicationService: get tag error: エラーメッセージ`
+    prismaMock.tag.findUnique.mockRejectedValue(expectedError)
+
+    expect.assertions(1)
+    await expect(tagService.getTag('invalid_tag_id')).rejects.toThrow(
+      errorMessage,
+    )
+  })
+})
+
+describe('削除されていないタグを全て取得する機能', () => {
+  test('削除されていないタグが取得されること', async () => {
+    const activeTags = [
+      createExpectedTag({ id: 'tag_id_1', name: 'Tag 1', color: 'red' }),
+      createExpectedTag({ id: 'tag_id_2', name: 'Tag 2', color: 'blue' }),
+      createExpectedTag({
+        id: 'tag_id_3',
+        name: 'Tag 3',
+        color: 'green',
+        deleted_at: new Date(),
+      }),
+    ]
+
+    prismaMock.tag.findMany.mockResolvedValue(activeTags)
+
+    const fetchedTags = await tagService.getTags()
+    fetchedTags.forEach((tag) => {
+      expect(tag.deleted_at).toBeNull()
+    })
+  })
+
+  test('削除されていないタグが取得されない場合、エラーが投げられること', async () => {
+    const expectedError = 'エラーメッセージ'
+    const errorMessage = `TagApplicationService: get tags error: エラーメッセージ`
+    prismaMock.tag.findMany.mockResolvedValue(expectedError)
+
+    expect.assertions(1)
+    await expect(tagService.getTags()).rejects.toThrow(
+      errorMessage,
+    )
+  })
 })
