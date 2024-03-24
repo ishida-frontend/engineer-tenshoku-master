f601ffbe4519a062a7ceb767c3181a0ceb81eb17
aimiyamaguchi <preticure112771@gmail.com>
2024-03-24 19:24:27 +0900

    エラーメッセージ修正

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -43,7 +43,7 @@ export default function Login() {
     })
     if (result.error) {
       toast({
-        title: 'このメールアドレスは登録されていません',
+        title: 'このメールアドレスは登録されていないかパスワードが誤っています',
         status: 'error',
         position: 'top',
         duration: 3000,
