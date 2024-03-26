1536360930e5465f2feb787a31eae7291ba178c1
aimiyamaguchi <preticure112771@gmail.com>
2024-03-26 17:41:08 +0900

    質問　useRouterの使い方

--- frontend/app/admin/advertisement/page.tsx
+++ frontend/app/admin/advertisement/page.tsx
@@ -1,11 +1,38 @@
-import { AdvertisementList } from "../../../components/admin/organisms/AdvertisementList"; 
-import { AdvertisementType } from "../../../types/AdvertisementType";
+'use client'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
+import { AdvertisementList } from '../../../components/admin/organisms/AdvertisementList'
+import { getServerSession } from 'next-auth'
+import { authOptions } from '../../api/auth/[...nextauth]/route'
 
-export default async function AdminAdvertisement(){
-  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/advertisement`, {
-    cache: 'no-cache'
-  }) 
-  const advertisements: AdvertisementType[] = await res.json()
+export default function AdminAdvertisement() {
+  const [advertisements, setAdvertisements] = useState([])
+  const router = useRouter()
 
-  return <AdvertisementList advertisements={advertisements}/>
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const session = await getServerSession(authOptions)
+        if (!session || session.user.isAdmin !== true) {
+          router.push('/404')
+          return
+        }
+
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_URL}/advertisement`,
+          {
+            cache: 'no-cache',
+          },
+        )
+        const data = await res.json()
+        setAdvertisements(data)
+      } catch (error) {
+        console.error('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+  }, [router])
+
+  return <AdvertisementList advertisements={advertisements} />
 }
