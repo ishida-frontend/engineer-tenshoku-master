ea6104394b4b6180663b26522fb1b32640205064
aimiyamaguchi <preticure112771@gmail.com>
2024-03-26 18:28:27 +0900

    質問　advertisementsデータの処理

--- frontend/app/admin/advertisement/page.tsx
+++ frontend/app/admin/advertisement/page.tsx
@@ -1,20 +1,28 @@
 'use client'
 import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/navigation'
 import { AdvertisementList } from '../../../components/admin/organisms/AdvertisementList'
+import { AdvertisementType } from '../../../types'
 import { getServerSession } from 'next-auth'
 import { authOptions } from '../../api/auth/[...nextauth]/route'
 
 export default function AdminAdvertisement() {
-  const [advertisements, setAdvertisements] = useState([])
+  const [advertisements, setAdvertisements] = useState<AdvertisementType[]>([]) // 広告データの状態を管理
   const router = useRouter()
 
   useEffect(() => {
     async function fetchData() {
       try {
         const session = await getServerSession(authOptions)
-        if (!session || session.user.isAdmin !== true) {
-          router.push('/404')
+        if (!session) {
+          console.log('!session', session)
+          router.push('/auth/login') // セッションがない場合はログインページにリダイレクト
+          return
+        }
+
+        if (!session.user.isAdmin) {
+          console.log('!session.user.isAdmin', session)
+          router.push('/404') // isAdminがfalseの場合は404ページにリダイレクト
           return
         }
 
@@ -24,8 +32,10 @@ export default function AdminAdvertisement() {
             cache: 'no-cache',
           },
         )
-        const data = await res.json()
-        setAdvertisements(data)
+        const data: AdvertisementType[] = await res.json()
+        setAdvertisements(data) // 広告データをセット
+        console.log('data', data)
+        console.log('session', session)
       } catch (error) {
         console.error('Error fetching data:', error)
       }
